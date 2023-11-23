21b446d4540c94d2c77c8c5cc151b18d6752417c
Robson Oliveira <robsonlnx@gmail.com>
2023-11-22 23:16:49 -0300

    Resolvido os 4 primeiros da lista de estrutura sequencial da lista pytnon brasil

---
+++ estrutura_sequencial/ex003.py
@@ -0,0 +1,10 @@
+"""Faça um Programa que peça dois números e imprima a soma."""
+
+
+first_number = int(input('Informe o primeiro número: '))
+second_number = int(input('Informe o segundo número: '))
+
+sum = first_number + second_number
+
+print(f'A soma entre {first_number} + {second_number} = {sum}')
+
---
+++ estrutura_sequencial/ex004.py
@@ -0,0 +1,11 @@
+"""Faça um Programa que peça as 4 notas bimestrais e mostre a média."""
+
+nota_1 = float(input('Primeira nota: '))
+nota_2 = float(input('Segunda nota: '))
+nota_3 = float(input('Terceira nota: '))
+nota_4 = float(input('Quarta nota: '))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print(f'A média entre as notas é de {media}')
+
