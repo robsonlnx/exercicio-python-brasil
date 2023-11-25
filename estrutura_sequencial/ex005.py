3c208e2f820bcb2ab21cd9c3f42b698f266ab67c
Robson Oliveira <robsonlnx@gmail.com>
2023-11-24 23:27:07 -0300

    Adicionado resolução exercicio 5 de estrutura sequencial

---
+++ estrutura_sequencial/ex005.py
@@ -0,0 +1,8 @@
+"""Faça um Programa que converta metros para centímetros."""
+
+valor_em_metros = int(input('Valor em metros: '))
+centimetros = valor_em_metros * 100
+
+print(f'O valor de {valor_em_metros} convertido em centimetros é igual a {centimetros}')
+
+
