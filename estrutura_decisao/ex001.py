5507bee8cc390be9c362df3df11535b048983dd1
Robson Oliveira <robsonlnx@gmail.com>
2023-12-10 14:19:06 -0300

    primeiro exercicio da estrutura de decisao

---
+++ estrutura_decisao/ex001.py
@@ -0,0 +1,22 @@
+'''Faça um Programa que peça dois números e imprima o maior deles.'''
+
+
+primeiro_numero = float(input('Primeiro número: '))
+segundo_numero = float(input('Segundo número: '))
+
+if primeiro_numero > segundo_numero:
+    print(f'O número {primeiro_numero} é maior. ')
+elif segundo_numero > primeiro_numero:
+    print(f'O número {segundo_numero} é maior. ')
+else:
+    print('Não existe maior. Números iguais')
+
+
+
+
+
+
+
+
+
+
